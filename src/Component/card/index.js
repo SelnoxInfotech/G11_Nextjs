2cbe76851bfaf6f5174edf3358e72798b86654eb
SelnoxInfotech <Selnox93@gmail.com>
2024-03-05 19:10:52 +0530

    fsdf

--- src/Component/card/index.js
+++ src/Component/card/index.js
@@ -44,93 +44,88 @@ const Card = ({ props, query }) => {
     const handlelessImage = () => {
         setNext(next - imagePerRow);
     };
- 
-        React.useEffect(() => {
+    React.useEffect(() => {
+        if (data === undefined) {
+            axios.get('/api/utils/getpostbycategory')
+                .then(response => {
+                    setdata(response.data)
+                })
+        }
+    })
+    return (
+        <div className='container-fluid center'>
+            <div className={`${"row"} ${style.Breaking_new}`}>
+                <div className={`col-12 ${style.breaking_news_hed}`}>
+                    <h1> Cricket Breaking News ON TRENDING TOPICS</h1>
+                </div>
 
-            if (data === undefined) {
+                {
+                    data?.slice(0, next)?.map((breakingnews, index) => {
+                        return (
 
-                axios.get('/api/utils/getpostbycategory')
-                    .then(response => {
-                        setdata(response.data)
-                    })
+                            <div className={`col-xs-12 col-sm-6 col-md-3  ${style.Breaking_news_gap}`} key={index}>
+                                <div className="card1 card">
+                                    <div className="video text-center">
 
-            }
-        })
+                                        <div className={`${"col"} ${style.ShareOption}`}>
+                                            <RWebShare
+                                                data={{
+                                                    url: `http://weedx.site/cricket-breaking-news/${modifystr(breakingnews?.urlslug !== null ? breakingnews?.urlslug?.toLowerCase() : breakingnews?.Title)}/${breakingnews.id}`
 
-        return (
-            <div className='container-fluid center'>
-                <div className={`${"row"} ${style.Breaking_new}`}>
-                    <div className={`col-12 ${style.breaking_news_hed}`}>
-                        <h1> Cricket Breaking News ON TRENDING TOPICS</h1>
-                    </div>
+                                                }}
+                                                onClick={() => console.log("shared successfully!")}
+                                            >
+                                                <Button className={`${style.ShareButton}`}>
+                                                    <BsFillShareFill color='#c2121c'></BsFillShareFill>
+                                                </Button>
+                                            </RWebShare>
 
-                    {
-                        data?.slice(0, next)?.map((breakingnews, index) => {
-                            return (
-
-                                <div className={`col-xs-12 col-sm-6 col-md-3  ${style.Breaking_news_gap}`} key={index}>
-                                    <div className="card1 card">
-                                        <div className="video text-center">
-
-                                            <div className={`${"col"} ${style.ShareOption}`}>
-                                                <RWebShare
-                                                    data={{
-                                                        url: `http://weedx.site/cricket-breaking-news/${modifystr(breakingnews?.urlslug !== null ? breakingnews?.urlslug?.toLowerCase() : breakingnews?.Title)}/${breakingnews.id}`
-
-                                                    }}
-                                                    onClick={() => console.log("shared successfully!")}
-                                                >
-                                                    <Button className={`${style.ShareButton}`}>
-                                                        <BsFillShareFill color='#c2121c'></BsFillShareFill>
-                                                    </Button>
-                                                </RWebShare>
-
+                                        </div>
+                                        <Link className={`${style.hovereffect}`} href={`/${query}/${modifystr(breakingnews?.urlslug !== null ? breakingnews?.urlslug?.toLowerCase() : breakingnews?.Title)}/${breakingnews.id}`} >
+                                            <Image className={style.News_image} loader={imageLoader} src={`${breakingnews.Image}`} height={10} width={100} alt="news_image" quality={100} />
+                                            <div className={style.News_image_title}>
+                                                <h2 className={`card-text  ${style.card_text}`}>{breakingnews.Title.slice(0, 80)}</h2>
                                             </div>
-                                            <Link className={`${style.hovereffect}`} href={`/${query}/${modifystr(breakingnews?.urlslug !== null ? breakingnews?.urlslug?.toLowerCase() : breakingnews?.Title)}/${breakingnews.id}`} >
-                                                <Image className={style.News_image} loader={imageLoader} src={`${breakingnews.Image}`} height={10} width={100} alt="news_image" quality={100} />
-                                                <div className={style.News_image_title}>
-                                                    <h2 className={`card-text  ${style.card_text}`}>{breakingnews.Title.slice(0, 80)}</h2>
-                                                </div>
-                                            </Link>
-                                            <div className={`col-12 ${style.viewCount}`}>
-                                                <div className={`col-6 ${style.viewCounteye}`}>
-                                                    {/* <AiFillEye></AiFillEye>  <span>{breakingnews?.ViewCount} view</span> */}
-                                                </div>
-                                                <div className={`col-6 ${style.ViewCountDate}`}>
-                                                    <p >{breakingnews.created.slice(0, 10)}</p>
-                                                </div>
+                                        </Link>
+                                        <div className={`col-12 ${style.viewCount}`}>
+                                            <div className={`col-6 ${style.viewCounteye}`}>
+                                                {/* <AiFillEye></AiFillEye>  <span>{breakingnews?.ViewCount} view</span> */}
+                                            </div>
+                                            <div className={`col-6 ${style.ViewCountDate}`}>
+                                                <p >{breakingnews.created.slice(0, 10)}</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
 
-                            )
-                        })
+                        )
+                    })
 
-                    }
-                    <div className={`${'row'} ${style.BreakingButton}`}>
-                        <div className='col-12 ' id='Buttongap'>
-                            {next < props?.length && (
-                                <button className="btn readleft" onClick={handleMoreImage}
-                                >
-                                    Load more
-                                </button>
-                            )}
-                            {next < props?.length && (
-                                <button className={next <= 5 ? 'hidden' : "btn readleft"} onClick={handlelessImage}
-                                >
-                                    Read Less
-                                </button>
-                            )}
-                        </div>
-
+                }
+                <div className={`${'row'} ${style.BreakingButton}`}>
+                    <div className='col-12 ' id='Buttongap'>
+                        {next < props?.length && (
+                            <button className="btn readleft" onClick={handleMoreImage}
+                            >
+                                Load more
+                            </button>
+                        )}
+                        {next < props?.length && (
+                            <button className={next <= 5 ? 'hidden' : "btn readleft"} onClick={handlelessImage}
+                            >
+                                Read Less
+                            </button>
+                        )}
                     </div>
+
                 </div>
-
             </div>
-        )
-    }
+
+        </div>
+    )
+}
 
 
 
-    export default Card
+export default Card
