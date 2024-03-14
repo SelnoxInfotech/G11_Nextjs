5bfded13e0211148ef95dc4e01c97a005d3b33bb
SelnoxInfotech <Selnox93@gmail.com>
2024-03-14 19:23:34 +0530

    ergergdfgdfg

--- src/pages/api/Rss/sitemapmatchpreview.xml.js
+++ src/pages/api/Rss/sitemapmatchpreview.xml.js
@@ -33,10 +33,10 @@ export async function generateRssXml() {
             if (html) {
                 const $ = cheerio.load(html);
                 const firstParagraph = $('p').first();
-                console.log(firstParagraph)
+                console.log(firstParagraph.text().split(":")[1])
                 if (firstParagraph) {
                     h.push({
-                        url1: modifystr(firstParagraph.text().replace(firstParagraph.text().slice(0, firstParagraph.text().indexOf(":") + 1), "")),
+                        url1: firstParagraph.text().split(":")[1],
                         id: rssData[index].id,
                         title: rssData[index].title
                     });
@@ -55,11 +55,22 @@ export async function generateRssXml() {
 export default async function handler(req, res) {
     try {
         const rssXml = await generateRssXml();
-        console.log(rssXml);
-        // res.setHeader('Content-Type', 'text/xml');
-        // res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate'); // Cache the feed for 24 hours
-        // res.write(rssXml);
-        // res.end();
+        const sitemapTeamguide = `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+           ${rssXml.map((url) => `
+                     <url>
+                      <loc>https://g11prediction.com/latest-match/cricket-predictions/match-preview//${modifystr(url.title)}/${url.url1}/${url.id}</loc>
+                     <changefreq>daily</changefreq>
+                      <priority>0.7</priority>
+                     </url>
+             `).join('')}
+               </urlset>`;
+     
+        res.setHeader('Content-Type', 'text/xml');
+        res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate'); // Cache the feed for 24 hours
+        console.log(sitemapTeamguide)
+        res.write(sitemapTeamguide);
+        res.end();
     } catch (error) {
         console.error('Error generating RSS feed:', error);
         res.status(500).end('Internal Server Error');
